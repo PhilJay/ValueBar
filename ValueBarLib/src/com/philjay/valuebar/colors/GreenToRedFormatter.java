b632d403b7c34ca5be6be0eef9ae0c4fe24780fc
Philipp Jahoda <phil.jahoda@gmail.com>
2014-10-25 11:59:57 +0200

    Minor fix.

--- ValueBarLib/src/com/philjay/valuebar/colors/GreenToRedFormatter.java
+++ ValueBarLib/src/com/philjay/valuebar/colors/GreenToRedFormatter.java
@@ -8,7 +8,7 @@ public class GreenToRedFormatter implements BarColorFormatter {
     @Override
     public int getColor(float value, float maxVal, float minVal) {
         
-        float hsv[] = new float[] { ((120f * ((maxVal-minVal) - value)) / (maxVal-minVal)), 1f, 1f };
+        float hsv[] = new float[] { ((120f * ((maxVal-minVal) - (value-minVal))) / (maxVal-minVal)), 1f, 1f };
 
         return Color.HSVToColor(hsv);
         
